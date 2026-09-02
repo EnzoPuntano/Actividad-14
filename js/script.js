b649b243df52c23a9a7168bc0af2e6476769f0a5
EnzoPuntano <enzo.puntano.t1vl@gmail.com>
2026-09-02 18:31:36 -0300

    Estructura completa

---
+++ js/script.js
@@ -0,0 +1,7 @@
+new QRCode(document.getElementById("qrcode"), {
+            text: "https://github.com/julian-agustin-gonzalez/RadarDeObra",
+            width: 200,
+            height: 200,
+            colorDark: "#1D1E1E",
+            colorLight: "#FFFFFF"
+        });
